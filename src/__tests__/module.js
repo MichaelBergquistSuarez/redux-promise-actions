0cac573b2b77021acaeced3fa88fd3507c5ab38f
michaelbergquistsuarez <michael.suarez.mail@gmail.com>
2017-05-02 10:20:07 +0200

    Fixed test dependency path

--- src/__tests__/module.js
+++ src/__tests__/module.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import module from '../../index';
+import module from '../';
 
 test('exports middleware', t => t.is(typeof module.middleware, 'function'));
 test('exports handlePromiseAction', t => t.is(typeof module.handlePromiseAction, 'function'));
